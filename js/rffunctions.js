c41194345d18d176b1e6349c4d8d8d6143580009
Ross Fitch <N0810909@my.ntu.ac.uk>
2020-01-03 13:29:32 +0000

    fixed

--- js/rffunctions.js
+++ js/rffunctions.js
@@ -2,6 +2,7 @@ const numberSlides = document.getElementsByClassName("slides");
 const numberText = document.getElementsByClassName("slideText");
 const thumbs = document.getElementsByClassName("thumb");
 var accounts = new Array();
+var exists = false;
 var currSlide = 0;
 var allowSubmit = false;
 timedSlide();
@@ -75,7 +76,7 @@ function captchaExpiry() {
 }
 
 function SubmitAcc() {
-    accounts = JSON.parse(localStorage.getItem('txtValues'));
+    accounts = loadAcc(accounts);
     if (allowSubmit) {
         accounts.push([document.getElementById('preEmail').value, document.getElementById('password').value]);
         localStorage.setItem('txtValues', JSON.stringify(accounts));
@@ -86,8 +87,8 @@ function SubmitAcc() {
 }
 
 function checkAcc() {
-    var exists = false;
-    accounts = JSON.parse(localStorage.getItem('txtValues'));
+    exists = false;
+    accounts = loadAcc(accounts);
     for(var i = 0; i < accounts.length; i++) {
         if(accounts[i].includes(document.getElementById('email').value) && accounts[i].includes(document.getElementById('existingPass').value))
             exists = true;
@@ -96,6 +97,15 @@ function checkAcc() {
         document.getElementById('email').setCustomValidity("");
     else 
         document.getElementById('email').setCustomValidity("Invalid email or password");
+}
+
+function loadAcc(accounts) {
+    if(localStorage.getItem('txtValues') == null){
+        accounts = [];
+    }else{
+        accounts =  JSON.parse(localStorage.getItem('txtValues'));
+    }
+    return(accounts);
 }
 
 function checkValidation() {
