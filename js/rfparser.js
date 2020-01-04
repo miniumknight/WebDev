56c722c0f9f10ee2be47dd02b0bbc87a261531f5
Ross Fitch <N0810909@my.ntu.ac.uk>
2020-01-04 10:57:18 +0000

    Xml start

---
+++ js/rfparser.js
@@ -0,0 +1,8 @@
+var xmlhttp, xmlDoc;
+
+xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "./products.xml", false);
+xmlhttp.send();
+xmlDoc = xmlhttp.responseXML;
+document.getElementsById("maintext").innerHTML=
+xmlDoc.getElementsByTagName("product")[0].childNodes[0].nodeValue;
