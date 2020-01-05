95919f292beaf11b11d11779ce9e845f814afaf9
Ross Fitch <N0810909@my.ntu.ac.uk>
2020-01-05 16:19:01 +0000

    Changed file structure

---
+++ js/rfparser.js
@@ -0,0 +1,23 @@
+async: true;
+var xmlGet = new XMLHttpRequest();
+xmlGet.onload = function () {
+    console.log(xmlGet.responseXML.documentElement.nodeName);
+}
+xmlGet.onerror = function () {
+    console.log("Error while loading XML");
+}
+xmlGet.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+        loadXML(this)
+    }
+}
+xmlGet.open("GET", "products.xml");
+xmlGet.send();
+
+function loadXML(xml) {
+    xmlFile = xml.responseXML;
+    var textboxHead = document.getElementsByClassName("prodName")
+    for (var i = 0; i < textboxHead.length; i++) {
+        textboxHead.item(i).innerHTML = xmlFile.getElementsByTagName("name")[i].childNodes[0].nodeValue;
+    }
+}
