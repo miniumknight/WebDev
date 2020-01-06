b95c07604b400a222b4406cd8823ab642e53c110
Ross Fitch <N0810909@my.ntu.ac.uk>
2020-01-06 22:52:07 +0000

    Testing XPath

--- js/rfparser.js
+++ js/rfparser.js
@@ -86,7 +86,7 @@ function swapXML(button) {
                 content +=`
                 <h1 class="prodName">${xmlFile.getElementsByTagName("name")[i].childNodes[0].nodeValue}</h1>
                     <div class="prodTxtContainer">
-                        <p>${descResult.childNodes[0].nodeValue}</p>
+                        <p><br>${descResult.childNodes[0].nodeValue}</p>
                         <p class="prodPrice">Price: £${priceResult.childNodes[0].nodeValue}</p>
                         <p class="prodDate">Year: ${relResult.childNodes[0].nodeValue}</p>
                     </div>
