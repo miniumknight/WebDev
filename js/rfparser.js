50e83ae0e635fbe6ba57e23c942dc12745d13533
Ross Fitch <N0810909@my.ntu.ac.uk>
2020-01-06 00:05:33 +0000

    Testing XPath

--- js/rfparser.js
+++ js/rfparser.js
@@ -16,17 +16,19 @@ xmlGet.send();
 
 function loadXML(xml) {
     xmlFile = xml.responseXML;
-    var nameNode = xmlFile.evaluate('/productshop/product/name', xmlFile, null, XPathResult.ANY_TYPE, null);
-    var imageNode = xmlFile.evaluate('/productshop/product/image', xmlFile, null, XPathResult.ANY_TYPE, null);
-    var descNode = xmlFile.evaluate('/productshop/product/description', xmlFile, null, XPathResult.ANY_TYPE, null);
-    var releaseNode = xmlFile.evaluate('/productshop/product/release', xmlFile, null, XPathResult.ANY_TYPE, null);
-    var priceNode = xmlFile.evaluate('/productshop/product/price', xmlFile, null, XPathResult.ANY_TYPE, null);
-    var productContainer = [nameNode, imageNode,descNode, releaseNode, priceNode];
     var content=""; 
-
-    for (var i = 0; i < productContainer[0].length; i++) {
-        content += '<div class="mainText"><h1>' + productContainer[0][i] + '</h1></div>'
+    if (xmlFile.evaluate) {
+        var nameNode = xmlFile.evaluate('/productshop/product/name', xmlFile, null, XPathResult.ANY_TYPE, null);
+        var imageNode = xmlFile.evaluate('/productshop/product/image', xmlFile, null, XPathResult.ANY_TYPE, null);
+        var descNode = xmlFile.evaluate('/productshop/product/description', xmlFile, null, XPathResult.ANY_TYPE, null);
+        var releaseNode = xmlFile.evaluate('/productshop/product/release', xmlFile, null, XPathResult.ANY_TYPE, null);
+        var priceNode = xmlFile.evaluate('/productshop/product/price', xmlFile, null, XPathResult.ANY_TYPE, null);
+        var productContainer = [nameNode, imageNode, descNode, releaseNode, priceNode];
+        var result = productContainer.iterateNext();
+        while(result) {
+            content += '<div class="mainText"><h1>' + result[0].childNodes[0].nodeValue + '</h1></div>';
+            result = productContainer.iterateNext();
+        }
     }
-            
     document.getElementById("productList").innerHTML = content;
 }
