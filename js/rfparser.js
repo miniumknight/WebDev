aa66cb9f164a91dd4ddcded76de0e48953f1ad49
Ross Fitch <N0810909@my.ntu.ac.uk>
2020-01-06 00:31:01 +0000

    Testing XPath

--- js/rfparser.js
+++ js/rfparser.js
@@ -16,16 +16,29 @@ xmlGet.send();
 
 function loadXML(xml) {
     xmlFile = xml.responseXML;
-    var content=""; 
-    if (xmlFile.evaluate) {
-        var nameNode = xmlFile.evaluate('/productshop/product/name', xmlFile, null, XPathResult.ANY_TYPE, null);
-        var imageNode = xmlFile.evaluate('/productshop/product/image', xmlFile, null, XPathResult.ANY_TYPE, null);
-        var descNode = xmlFile.evaluate('/productshop/product/description', xmlFile, null, XPathResult.ANY_TYPE, null);
-        var releaseNode = xmlFile.evaluate('/productshop/product/release', xmlFile, null, XPathResult.ANY_TYPE, null);
-        var priceNode = xmlFile.evaluate('/productshop/product/price', xmlFile, null, XPathResult.ANY_TYPE, null);
-        var productContainer = [nameNode, imageNode, descNode, releaseNode, priceNode];
-
-        alert(productContainer[0].length)
+    var nameNode = xmlFile.evaluate('/productshop/product/name', xmlFile, null, XPathResult.ANY_TYPE, null);
+    var imageNode = xmlFile.evaluate('/productshop/product/image', xmlFile, null, XPathResult.ANY_TYPE, null);
+    var descNode = xmlFile.evaluate('/productshop/product/description', xmlFile, null, XPathResult.ANY_TYPE, null);
+    var releaseNode = xmlFile.evaluate('/productshop/product/release', xmlFile, null, XPathResult.ANY_TYPE, null);
+    var priceNode = xmlFile.evaluate('/productshop/product/price', xmlFile, null, XPathResult.ANY_TYPE, null);
+    var products = [nameNode, imageNode, descNode, releaseNode, priceNode, products];
+    /*var textboxHead = document.getElementsByClassName("prodName")
+    for (var i = 0; i < textboxHead.length; i++) {
+        textboxHead.item(i).innerHTML = xmlFile.getElementsByTagName("name")[i].childNodes[0].nodeValue;
+    }*/
+    
+    for (i = 0; i < products.length; i++) {
+        products[i].iterateNext();
     }
-    document.getElementById("productList").innerHTML = content;
+
+    var txt="";
+    if (xmlFile.evaluate) {
+        while (nameNode) {
+            txt += '<div class="mainText"><h1>' +  nameResult.childNodes[0].nodeValue + '</h1><p>' + products[i].childNodes[0].nodeValue + '</p></div>';
+            for (i = 0; i < products.length; i++) {
+                products[i].iterateNext();
+            }
+        }
+    }
+    document.getElementById("productList").innerHTML = txt;
 }
