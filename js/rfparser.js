4ea09442506ce63218a138307ba7620f9d50f3ac
Ross Fitch <N0810909@my.ntu.ac.uk>
2020-01-06 17:39:42 +0000

    Testing XPath

--- js/rfparser.js
+++ js/rfparser.js
@@ -55,7 +55,7 @@ function loadXML(xml) {
             xmlFile.getElementsByTagName("name")[i].childNodes[0].nodeValue + 
             '</h1><img class="prodImage" src="' + 
             xmlFile.getElementsByTagName("image")[i].childNodes[0].nodeValue + 
-            '"></img></div>';
+            '"></img><btn class ="prodInfo"></div>';
         }
         else {
             content +=
@@ -63,7 +63,7 @@ function loadXML(xml) {
             xmlFile.getElementsByTagName("name")[i].childNodes[0].nodeValue + 
             '</h1><img class="prodImage" src="' + 
             xmlFile.getElementsByTagName("image")[i].childNodes[0].nodeValue + 
-            '"></img></div></div>';
+            '"></img><btn class ="prodInfo"></div></div>';
         }
     }
 
