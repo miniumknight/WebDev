9bfab6f665dd46ee486c40bb063cfb5e48b62e99
Ross Fitch <N0810909@my.ntu.ac.uk>
2020-01-06 21:49:45 +0000

    Testing XPath

--- js/rfparser.js
+++ js/rfparser.js
@@ -85,9 +85,9 @@ function swapXML(button) {
             if (i == buttonNum) {
                 content +=`
                 <h1 class="prodName">${xmlFile.getElementsByTagName("name")[i].childNodes[0].nodeValue}</h1>
-                    <p>${descResult.childNodes[0].nodeValue}</p>
-                    <p class = "prodPrice">Price: £${priceResult.childNodes[0].nodeValue}</p>
-                    <p class = "prodDate">Year: ${relResult.childNodes[0].nodeValue}</p>
+                    <p class="prodDesc">${descResult.childNodes[0].nodeValue}</p>
+                    <p class="prodPrice">Price: £${priceResult.childNodes[0].nodeValue}</p>
+                    <p class="prodDate">Year: ${relResult.childNodes[0].nodeValue}</p>
                 <btn class="prodInfo" onclick="swapXML(this)" id="${i}">Back</btn>`;
             }
         }
