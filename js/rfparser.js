e75af59fb7e4655b8ea57bdd4832bb80a58c1dd7
Ross Fitch <N0810909@my.ntu.ac.uk>
2020-01-07 17:09:36 +0000

    Testing XPath

--- js/rfparser.js
+++ js/rfparser.js
@@ -75,11 +75,11 @@ function swapXML(button) {
         }
         else if (window.ActiveXObject || xmlGet.responseType == "msxml-document" || 'ActiveXObject' in window) {
             IEDoc = new ActiveXObject('Microsoft.XMLDOM');
-            IEDoc.loadXML(xmlGet);
+            IEDoc.loadXML(xmlGet.responseXML);
             descResult = IEDoc.selectNodes(descPath);
             priceResult = IEDoc.selectNodes(pricePath);
             relResult = IEDoc.selectNodes(relPath)
-            docResult = document.selectNodes(docPath);
+            docResult = document.getElementById(buttonNum);
         }
 
         for (var i = 0; i < products.length; i++) {
