d3ca18c2f127fbffd7349ff2a64b8056b0b93de6
Ross Fitch <N0810909@my.ntu.ac.uk>
2020-01-07 15:46:01 +0000

    Testing XPath

--- js/rffunctions.js
+++ js/rffunctions.js
@@ -1,5 +1,5 @@
 if('ActiveXObject' in window) {
-    alert("Sorry, you're running a very outdated which doesnt fully support XML please try using a newer browser to view this site!");
+    alert("Sorry, you're running a very outdated which doesnt fully support JS/XML please try using a newer browser to view this site!");
 }
 const numberSlides = document.getElementsByClassName("slides");
 const numberText = document.getElementsByClassName("slideText");
