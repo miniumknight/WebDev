03ffdf52203a9999b34410b52c3b78df21c2ec8e
Ross Fitch <rossafitch@hotmail.co.uk>
2020-01-16 20:46:32 +0000

    Commented code

--- js/rfparser.js
+++ js/rfparser.js
@@ -22,27 +22,27 @@ xmlReq.open("GET", "products.xml");
 xmlReq.send();
 
 function loadXML() {
-    var content = "";
+    var content = ""; //Empty string which will hold the HTML content of the page.
     var products = xmlFile.getElementsByTagName("product");
     
-    content += '<div class="header"><h1>All Products</h1></div>';
+    content += '<div class="header"><h1>All Products</h1></div>'; //Generate header.
 
     for(var i = 0; i < products.length; i++) { //For each product which exists within the XML file.
-        if (i%2 == 0 || i == 0) { //Check if even.
+        if (i%2 == 0 || i == 0) { //If even (Checks using modulus/Division remainder).
             content +=
             '<div class ="prodContainer">' +
-                '<div class="prodText" id="'+i+'">' +
+                '<div class="prodText" id="'+i+'">' + //Each div containing the XML data is given an ID number equal to the current product number when it is generated. This helps with changing the content later and with XPath queries.
                     '<h1 class="prodName">'+xmlFile.getElementsByTagName("name")[i].childNodes[0].nodeValue+'</h1>' +
                     '<img class="prodImage" src="'+xmlFile.getElementsByTagName("image")[i].childNodes[0].nodeValue+'">' +
-                    '<btn class="prodInfo" onclick="swapXML(this)" id="'+i+'">More Information</btn>' + 
+                    '<btn class="prodInfo" onclick="swapXML(this)" id="'+i+'">More Information</btn>' + //Each button is also given an ID number equal to the current product number when generated. This is so the program can find which button the user has clicked and perform the relevant function.
                 '</div>';
-        } //Generate content using HTML and XML for the left side (Due to half and half layout; Opens the 50/50 container div).
+        } //Generate content using HTML and XML for the left side (Due to half and half layout; Opens the set width container div for 50/50 elements).
         else { //If not even.
             content +=
                 '<div class="prodText" id="'+i+'">' +
                     '<h1 class="prodName">'+xmlFile.getElementsByTagName("name")[i].childNodes[0].nodeValue+'</h1>' +
                     '<img class="prodImage" src="'+xmlFile.getElementsByTagName("image")[i].childNodes[0].nodeValue+'">' +
-                    '<btn class="prodInfo" onclick="swapXML(this)" id="'+i+'">More Information</btn>' + 
+                    '<btn class="prodInfo" onclick="swapXML(this)" id="'+i+'">More Information</btn>' +
                 '</div>' +
             '</div>'
         } //Generate content using HTML and XML for the right side (Slightly different HTML; Closes the 50/50 container div).
@@ -54,29 +54,29 @@ function loadXML() {
 }
 
 function swapXML(button) {
-    var content = "";
-    var buttonNum = button.id; var selectorNum = (+buttonNum + 1);
-    var infoCheck = document.getElementsByClassName("prodText");
-    var products = xmlFile.getElementsByTagName("product");
+    var content = ""; //Empty string which will hold the HTML content of the invidual product container.
+    var buttonNum = button.id; var selectorNum = (+buttonNum + 1); //These variables are seperate to each other due to some aspects of the code requiring positive index notation (Cannot start at 0), whereas others use the actual index notation of the button.    
+    var infoCheck = document.getElementsByClassName("prodText"); //Variable which holds data for all of the div containers labelled "prodText".
+    var products = xmlFile.getElementsByTagName("product"); //Variable which holds data for all of the XML elements tagged as "product".
 
     var descPath = '/productshop/product['+selectorNum+']/description';
     var pricePath = '/productshop/product['+selectorNum+']/price';
-    var relPath = '/productshop/product['+selectorNum+']/release';
-    var docPath = '//div[contains(@id, "'+buttonNum+'")]';
+    var relPath = '/productshop/product['+selectorNum+']/release'; //Paths to be used with XPath, these variables use the index notation of the button which the user has clicked to find the correct path to the corresponding XML data.
+    var docPath = '//div[contains(@id, "'+buttonNum+'")]'; //This path uses the "contains" perameter, which means it will find every path which has an ID containing buttonNum. The path works because each element is given an ID when generated.
 
-    if(infoCheck[buttonNum].className === "prodText") {
+    if(infoCheck[buttonNum].className === "prodText") { //Check if the text container div has its original class name (So that the user can open and close the information). 
         var descNode = xmlFile.evaluate(descPath, xmlFile, null, XPathResult.ANY_TYPE, null);
         var priceNode = xmlFile.evaluate(pricePath, xmlFile, null, XPathResult.ANY_TYPE, null);
         var relNode = xmlFile.evaluate(relPath, xmlFile, null, XPathResult.ANY_TYPE, null);
-        var docNode = document.evaluate(docPath, document, null, XPathResult.ANY_TYPE, null);
+        var docNode = document.evaluate(docPath, document, null, XPathResult.ANY_TYPE, null); //Define all XPath queries using the previosuly declared paths. The null values are the resolver (HTML requires no namespace prefixes) and the result (Needs to produce a new result not use an existing one) properties.
 
         var descResult = descNode.iterateNext();
         var priceResult = priceNode.iterateNext();
         var relResult = relNode.iterateNext();
-        var docResult = docNode.iterateNext();
+        var docResult = docNode.iterateNext(); //Define new variables which are equal to the next iteration of the XPath queries (The result). This is done because the program must IterateNext() in order to fint the actual result of the search query, otherwise it will return null.
 
-        for (var i = 0; i < products.length; i++) {
-            if (i == buttonNum) {
+        for (var i = 0; i < products.length; i++) { //Iterate through all XML products.
+            if (i == buttonNum) { //Check if the index notation of the product is equal to the ID of the button clicked.
                 content +=
                 '<h1 class="prodName">'+xmlFile.getElementsByTagName("name")[i].childNodes[0].nodeValue+'</h1>' +
                     '<div class="prodTxtContainer fade-in-transition">' +
@@ -84,13 +84,13 @@ function swapXML(button) {
                         '<p class="prodPrice">Price: £'+priceResult.childNodes[0].nodeValue+'</p>' +
                         '<p class="prodDate">Year: '+relResult.childNodes[0].nodeValue+'</p>' +
                     '</div>' +
-                '<btn class="prodInfo" onclick="swapXML(this)" id="'+i+'">Back</btn>';
+                '<btn class="prodInfo" onclick="swapXML(this)" id="'+i+'">Back</btn>'; //Update the HTML in the content variable using XML containing product information (Rather than HTML & XML to display an image).
             }
         }
-        infoCheck[buttonNum].className += " open"
+        infoCheck[buttonNum].className += " open" //Update the class name of the container with the same ID as the button clicked so that it contains "open".
     }
 
-    else {
+    else { //If the text container div does not have its original class name (Contains "open").
         var docNode = document.evaluate(docPath, document, null, XPathResult.ANY_TYPE, null);
         var docResult = docNode.iterateNext();
 
@@ -100,23 +100,23 @@ function swapXML(button) {
                 '<h1 class="prodName">'+xmlFile.getElementsByTagName("name")[i].childNodes[0].nodeValue+'</h1>' +
                 '<img class="prodImage fade-in-transition" src="'+xmlFile.getElementsByTagName("image")[i].childNodes[0].nodeValue+'">' +
                     '<btn class="prodInfo" onclick="swapXML(this)" id="'+i+'">More Information</btn>' +
-                '</img>';
+                '</img>'; //Reset the content of the container so that it is the same as when it was originally generated.
             }
         }
-        infoCheck[buttonNum].className = "prodText";
+        infoCheck[buttonNum].className = "prodText"; //Reset the class of the container to what it was originally (Again, so that the user can open or close the information).
     } 
-    docResult.innerHTML = content;
+    docResult.innerHTML = content; //Once all the above code has finished running, set the HTML content of the text container div so that is equal to the previously generated content.
 }
 
-function checkXML() {
+function checkXML() { //Function which compares the value a user has input to the XML data and updates the validity of the corresponding input tag accordingly.
     var products = xmlReq.responseXML.getElementsByTagName("product");
-    for (i = 0; i < products.length; i++) {
-        if(document.getElementsByName("product")[0].value == xmlReq.responseXML.getElementsByTagName("name")[i].childNodes[0].nodeValue){
-            document.getElementsByName("product")[0].setCustomValidity("");
-            break;
+    for (i = 0; i < products.length; i++) { //Iterate through the products in the XML once again.
+        if(document.getElementsByName("product")[0].value == xmlReq.responseXML.getElementsByTagName("name")[i].childNodes[0].nodeValue){ //If the value input by the user exists in the XML.
+            document.getElementsByName("product")[0].setCustomValidity(""); //The input is valid (The product exists in the XML file).
+            break; //End the loop.
         }
-        else {
-            document.getElementsByName("product")[0].setCustomValidity("That product does not exist within our database! (Case and spacing sensitive)");
-        }
+        else { //The input is invalid (The product does not exist in the XML file).
+            document.getElementsByName("product")[0].setCustomValidity("That product does not exist within our database! (Case and spacing sensitive)"); //Tell the user that their input is invalid and why.
+        } 
     }
 }
